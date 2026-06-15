8abac8b5b7c17f513fc3641fac4a4700faf26f78
Cristian <cristian@example.com>
2026-06-14 20:30:28 -0500

    import createCliente de supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 // src/supabaseClient.js
-
+import { createClient } from '@supabase/supabase-js'
 // Al usar el script CDN del paso anterior, la variable 'supabase' se crea automáticamente de forma global
 const supabaseUrl = window.env?.SUPABASE_URL;
 const supabaseAnonKey = window.env?.SUPABASE_ANON_KEY;
