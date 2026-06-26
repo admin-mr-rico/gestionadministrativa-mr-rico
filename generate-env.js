dada5dd4c70e5614ece41a23ff8ec236ca399194
Cristian Eduardo Mora Manrique <administracionecoclean@gmail.com>
2026-06-26 16:24:26 +0200

    probando arreglo en generate-env.js linea 18 y 19 y en vercel.json en linea 19

--- generate-env.js
+++ generate-env.js
@@ -15,8 +15,8 @@ if (fs.existsSync(envPath)) {
   });
 }
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL_LOCAL || process.env.SUPABASE_URL_LOCAL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_LOCAL || process.env.SUPABASE_ANON_KEY_LOCAL || '';
 
 const outputPath = path.join(__dirname, 'env.js');
 const contents = `window.SUPABASE_URL = '${supabaseUrl.replace(/'/g, "\\'")}';\nwindow.SUPABASE_ANON_KEY = '${supabaseAnonKey.replace(/'/g, "\\'")}';\n`;
